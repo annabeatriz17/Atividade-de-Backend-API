988b456cbd2bb4c8e68fdcf69c52c271fb7ed287
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-29 10:34:25 -0300

    create report

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,1 +1,36 @@
 const PDFDocument = require("pdfkit");
+
+const heroesModel = require("../models/heroesModel");
+
+const exportHeroesPDF = async (req, res) => {
+    try {
+        const heroes = await heroesModel.getAllHeroes();
+
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader("Content-Disposition",  "inline; filename=wizards.pdf");
+
+        const doc = new PDFDocument();
+        doc.pipe(res);
+
+        //Titulo
+        doc.fontSize(20).text("Relatório de Heróis", {align: "center"});
+        doc.moveDown();
+
+        //Cabeçalho
+        doc.fontSize(12).text("Id| Nome do Heroi | Editora", {underline: true});
+        doc.moveDown(0.5);
+
+         //Add dados dos bruxos
+        heroes.forEach((hero) => {
+            doc.text (
+                `${hero.id} | ${hero.name_characters} | ${hero.publishers_name}`
+            );
+        });
+
+        doc.end();
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao gerar o PDF"});
+    }
+};
+
+module.exports = { exportHeroesPDF };
---
+++ src/routes/reportRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const reportController = require("./../controllers/reportController");
+const apiKeyMiddleware = require("../config/apiKey")
+
+router.use(apiKeyMiddleware);
+
+
+//Rota para exportar PDF
+router.get("/report/pdf", reportController.exportHeroesPDF);
+
+
+module.exports = router;
