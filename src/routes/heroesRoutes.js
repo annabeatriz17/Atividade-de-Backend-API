df4a01117fbd1c0eb2eca235abe3d53b24937eeb
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-29 09:35:24 -0300

    Update heroesRoutes.js

--- src/routes/heroesRoutes.js
+++ src/routes/heroesRoutes.js
@@ -6,7 +6,7 @@ const upload = require("../config/upload");
 
 router.get("/heroes", heroesController.getAllHeroes);
 router.get("/heroes/:id", heroesController.getHeroesById);
-router.post("/heroes", heroesController.createHero);
+router.post("/heroes", upload.single("photo"), heroesController.createHero);
 router.put("/heroes/:id", heroesController.updateHero);
 router.delete("/heroes/:id", heroesController.deleteHero);
 
